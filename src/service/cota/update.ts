fbd6baf4ecd7a9cd4e541dab3e85aaf049cecbc3
duanyytop <duanyytop@gmail.com>
2022-02-15 14:16:37 +0800

    feat: Add update cota service

---
+++ src/service/cota/update.ts
@@ -0,0 +1,40 @@
+import { scriptToHash } from '@nervosnetwork/ckb-sdk-utils'
+import { CotaNft, Service, UpdateReq } from '../..'
+import { FEE, TestnetDeployment } from '../../constants'
+
+export const updateCotaNFT = async (service: Service, cotaLock: CKBComponents.Script, cotaNfts: CotaNft[], fee = FEE) => {
+  const cotaType = TestnetDeployment.CotaTypeScript
+  const cotaCells = await service.collector.getCells(cotaLock, cotaType)
+  if (!cotaCells || cotaCells.length === 0) {
+    throw new Error("Cota cell doesn't exist")
+  }
+  const cotaCell = cotaCells[0]
+  const inputs = [
+    {
+      previousOutput: cotaCell.outPoint,
+      since: '0x0',
+    },
+  ]
+  const outputs = [cotaCell.output]
+  outputs[0].capacity = `0x${(BigInt(outputs[0].capacity) - fee).toString(16)}`
+  const updateReq: UpdateReq = {
+    lockHash: scriptToHash(cotaLock),
+    nfts: cotaNfts,
+  }
+  const { smtRootHash, updateSmtEntry } = await service.aggregator.generateUpdateCotaSmt(updateReq)
+  const outputsData = [`0x00${smtRootHash}`]
+  const cellDeps = [TestnetDeployment.CotaTypeDep]
+  const rawTx = {
+    version: '0x0',
+    cellDeps,
+    headerDeps: [],
+    inputs,
+    outputs,
+    outputsData,
+    witnesses: [],
+  }
+  rawTx.witnesses = rawTx.inputs.map((_, i) =>
+    i > 0 ? '0x' : { lock: '', inputType: `0x05${updateSmtEntry}`, outputType: '' },
+  )
+  return rawTx
+}
