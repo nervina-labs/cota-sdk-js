bad9773f8b5cc44e7cb8182f77954ab15d5e0c76
duanyytop <duanyytop@gmail.com>
2022-03-29 13:59:04 +0800

    chore: Add is_claimed example

--- example/aggregator.ts
+++ example/aggregator.ts
@@ -28,6 +28,14 @@ const run = async () => {
     cotaId: '0xb22585a8053af3fed0fd39127f5b1487ce08b756',
   })
   console.log(JSON.stringify(defineInfo))
+
+  const isClaimed = await aggregator.isClaimed({
+    lockScript:
+      '0x490000001000000030000000310000009bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce80114000000dc70f33de86fdf381b4fc5bf092bb23d02774801',
+    cotaId: '0xb22585a8053af3fed0fd39127f5b1487ce08b756',
+    tokenIndex: '0x00000000',
+  })
+  console.log(JSON.stringify(isClaimed))
 }
 
 run()
